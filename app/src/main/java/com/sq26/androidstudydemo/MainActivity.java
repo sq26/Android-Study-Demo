c566b155fe73843d8254f3759a38e2649a674327
syq <sq26@sohu.com>
2019-01-03 11:37:27 +0800

    项目初始化

---
+++ app/src/main/java/com/sq26/androidstudydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sq26.androidstudydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
